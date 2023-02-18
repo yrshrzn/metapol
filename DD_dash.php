9f4889284a77706d597e7a52560fbf3d8cd75086
yrshrzn <111380483+yrshrzn@users.noreply.github.com>
2023-02-18 16:46:38 -0600

    Create DD_dash.php

---
+++ DD_dash.php
@@ -0,0 +1,15 @@
+<div class="dashboard-container">
+  <div class="dashboard-header">
+    <h1>Due Diligence Portal Dashboard</h1>
+  </div>
+  <div class="dashboard-content">
+    <div class="dashboard-section">
+      <h2>Companies</h2>
+      <ul id="company-list"></ul>
+    </div>
+    <div class="dashboard-section">
+      <h2>Proposals</h2>
+      <ul id="proposal-list"></ul>
+    </div>
+  </div>
+</div>
